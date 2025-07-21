b949521f01f67e5e2ee2197f2fd574c53507bca4
Celal Karakoç <c.karakoc@live.nl>
2025-07-21 11:37:35 +0200

    Added clean script. Fixed assets showing on github pages but not locally.

--- projects/shared-404/src/lib/shared-404.ts
+++ projects/shared-404/src/lib/shared-404.ts
@@ -10,13 +10,13 @@ import { NgStyle } from '@angular/common';
     <div class="flex h-screen w-full items-center justify-center bg-cover bg-center bg-no-repeat"
          [ngStyle]="{ 'background-image': backgroundImage }">
       <img class="max-h-full max-w-full"
-           src="assets/shared-404/pantzer_turtle_404.png"
+           src="shared-404/assets/pantzer_turtle_404.png"
            alt="404"/>
     </div>
   `,
   styleUrl: './shared-404.css'
 })
 export class Shared404 {
-  backgroundImage: string = 'url("assets/shared-404/war.jpg")';
+  backgroundImage: string = 'url("shared-404/assets/war.jpg")';
 
 }
