77a28b7e812c3a8d43076a12a6a36363a34100aa
Celal Karakoç <c.karakoc@live.nl>
2025-07-21 21:44:53 +0200

    Added masonry grid as homepage.

--- projects/main/src/app/home/home.ts
+++ projects/main/src/app/home/home.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, computed, ElementRef, signal, ViewChild } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  @ViewChild('masonry') masonry!: ElementRef<HTMLDivElement>;
 
+  isElementScrolled = signal<boolean>(false);
+  isWindowScrolled = signal<boolean>(false);
+  isScrollAtTop = computed(() => !this.isElementScrolled() && !this.isWindowScrolled())
+
+  checkScrollTop(el: HTMLElement): void {
+    this.isElementScrolled.set(el.scrollTop > 50);
+    this.isWindowScrolled.set(window.scrollY > 50);
+  }
+
+  scrollToTop(el: HTMLElement): void {
+    if (this.isElementScrolled())
+      el.scrollTo({ top: 0, behavior: 'smooth' });
+    else
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  protected readonly faChevronCircleUp = faChevronCircleUp;
 }
