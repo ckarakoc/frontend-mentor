4789e1be31aeaea339abd2b07a8a0a0b2b5aefa1
Celal Karakoç <c.karakoc@live.nl>
2025-07-28 23:03:12 +0200

    Added dist for GH-pages.

---
+++ dist/four-card-feature/index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class FourCardFeature {
+    static ɵfac: i0.ɵɵFactoryDeclaration<FourCardFeature, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<FourCardFeature, "lib-four-card-feature", never, {}, {}, never, never, true, never>;
+}
+
+export { FourCardFeature };
